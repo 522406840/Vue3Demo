293bbe47a90af800fd59685372b70eecd922144b
PANGFZ <feng.zhi.pang@ibm.com>
2021-06-17 16:59:22 +0800

    创建子模块

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <span
+    v-for="item in nums"
+    :key="item"
+    v-bind:title="message"
+    @click="onClick"
+  >
+    测试绑定属性
+  </span>
+  <div v-once>
+    父级数据 {{ count }}
+    <div>
+      子集数据 {{count}}
+    </div>
+  </div>
+  <div>
+    同级数据 {{ count }}
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      message: '鼠标悬浮效果',
+      nums: [1, 2, 3],
+      count: 0,
+    }
+  },
+  mounted() {
+    
+  },
+  methods: {
+    onClick() {
+      alert(123);
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
